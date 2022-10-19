5cbdcba8b8127f943b38405164f5e81d1e695f2c
Dopezayne <zainabaminu100@gmail.com>
2022-10-19 04:18:31 -0700

    Header

--- main.h
+++ main.h
@@ -18,11 +18,25 @@
 #define S_LONG 2
 #define S_SHORT 1
 
+/**
+ * struct fmt - Struct op
+ *
+ * @fmt: the format
+ * @fn the function associated
+ */
+
 struct fmt
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
 };
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: the format
+ * @fm_t: the function associated
+ */
 
 typedef struct fmt fmt_t;
 
